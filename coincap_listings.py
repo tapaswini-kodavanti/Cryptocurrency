5276a5a734f384cfe4e8d9574741d1875b42c2f1
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2021-10-19 21:50:36 -0500

    Update coincap_listings.py

--- coincap_listings.py
+++ coincap_listings.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 api_key = [
-    "95551983-47f7-46d6-b224-63f7fddb43e8"
+    "Insert your key"
 ]
 
 listing_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY="+api_key[0]
