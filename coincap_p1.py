a6aa4c7bfa0b8bb09a9ae1795c57476d01d85578
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2021-10-19 21:50:58 -0500

    Update coincap_p1.py

--- coincap_p1.py
+++ coincap_p1.py
@@ -8,7 +8,7 @@ from colorama import Fore, Back, Style
 convert = "USD"
 
 api_key = [
-    "95551983-47f7-46d6-b224-63f7fddb43e8"
+    "Insert your key"
 ]
 
 listing_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY="+api_key[0] + "&convert=" + convert
